f9c08d107a63bb6ffd9ce93816f5b37ad6060c89
dudely3 <addison.re@gmail.com>
2023-12-18 23:47:01 -0400

    initial commit of template

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestCrossPlatformApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestCrossPlatformApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
